c58041ced3d4902530efa23c4c433c429948d1bb
Seoghyun Kang <sh0708.kang@samsung.com>
2020-03-11 13:37:37 +0900

    Init a Browser tutorial project

---
+++ Mobile/TizenSchool/Browser/Browser/Browser/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Browser
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void WebView_Navigated(object sender, WebNavigatedEventArgs e)
+        {
+            LoadingIcon.IsVisible = false;
+        }
+
+        private void WebView_Navigating(object sender, WebNavigatingEventArgs e)
+        {
+            LoadingIcon.IsVisible = true;
+        }
+    }
+}
---
+++ Mobile/TizenSchool/Browser/Browser/Browser/MainPageViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Xamarin.Forms;
+
+namespace Browser
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        private string _entryUrl;
+        public string EntryUrl {
+            get => _entryUrl;
+            set
+            {
+                _entryUrl = value;
+                OnPropertyChanged();
+            }
+        }
+        private string _webViewUrl;
+
+        public Command ExecuteCommand { get; set; }
+
+        public string WebViewUrl
+        {
+            get => _webViewUrl;
+            set {
+                EntryUrl = value;
+                _webViewUrl = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainPageViewModel()
+        {
+            WebViewUrl = "https://www.google.com";
+            ExecuteCommand = new Command(ChangeWebUrl);
+        }
+
+        private void ChangeWebUrl()
+        {
+            WebViewUrl = EntryUrl;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
